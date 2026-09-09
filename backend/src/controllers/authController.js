d0cb4aa967efa1964c8d0f3ccb2b762676706ec3
Sharon812 <sharusharon1110@gmail.com>
2026-09-09 15:36:03 +0530

    setted up login api and also along with it added jwt setted up jwt authentication as well

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
-import bycrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 //function to register user
 export const registerUser = async (req, res) => {
@@ -23,7 +24,7 @@ export const registerUser = async (req, res) => {
       });
     }
 
-    const hashedPassword = await bycrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
       name,
@@ -46,6 +47,60 @@ export const registerUser = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Internal server error",
+    });
+  }
+};
+
+//function for login user
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server error",
     });
   }
 };
--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, login, getMe } from "../controllers/authController.js";
+import protectApis from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", login);
 
 export default router;
